4717fb7817424c257f28b85085cc79c1639bc192
David John <davidajohn@gmail.com>
2021-06-14 16:46:33 +0100

    Added project references and basic model classes

---
+++ FotoStorio.Shared/Models/Product.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FotoStorio.Shared.Models
+{
+    public class Product : BaseEntity
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public string ImageUrl { get; set; }
+        public int BrandId { get; set; }
+        public Brand Brand { get; set; }
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
+        public bool IsAvailable { get; set; } = true;
+    }
+}
