7b45fd06c8bea85ae0e5505aeef48456b16f7231
David John <davidajohn@gmail.com>
2021-06-17 16:06:47 +0100

    Added logging to the product service

--- FotoStorio.Client/Services/ProductService.cs
+++ FotoStorio.Client/Services/ProductService.cs
@@ -4,15 +4,19 @@ using System.Net.Http.Json;
 using System.Threading.Tasks;
 using FotoStorio.Client.Contracts;
 using FotoStorio.Shared.Models;
+using Microsoft.Extensions.Logging;
 
 namespace FotoStorio.Client.Services
 {
     public class ProductService : IProductService
     {
         private readonly IHttpClientFactory _httpClient;
-        public ProductService(IHttpClientFactory httpClient)
+        private readonly ILogger<ProductService> _logger;
+
+        public ProductService(IHttpClientFactory httpClient, ILogger<ProductService> logger)
         {
             _httpClient = httpClient;
+            _logger = logger;
         }
 
         public async Task<Product> GetProductByIdAsync(int id)
@@ -26,6 +30,7 @@ namespace FotoStorio.Client.Services
             }
             catch (HttpRequestException ex)
             {
+                _logger.LogError(ex.StatusCode + " " + ex.Message);
                 throw new HttpRequestException(ex.Message);
             }
         }
@@ -35,12 +40,13 @@ namespace FotoStorio.Client.Services
             try
             {
                 var client = _httpClient.CreateClient("FotoStorioAPI");
-                var products = await client.GetFromJsonAsync<List<Product>>("products");
+                var products = await client.GetFromJsonAsync<List<Product>>("product");
 
                 return products;
             }
             catch (HttpRequestException ex)
             {
+                _logger.LogError(ex.StatusCode + " " + ex.Message);
                 throw new HttpRequestException(ex.Message);
             }
         }
