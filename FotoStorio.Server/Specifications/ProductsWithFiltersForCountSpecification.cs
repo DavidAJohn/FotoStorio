fa40f306aca2397e746be68329a763382f32d153
David John <davidajohn@gmail.com>
2021-06-20 22:55:19 +0100

    Added a product count specification class

---
+++ FotoStorio.Server/Specifications/ProductsWithFiltersForCountSpecification.cs
@@ -0,0 +1,17 @@
+﻿using FotoStorio.Server.Helpers;
+using FotoStorio.Shared.Models;
+
+namespace FotoStorio.Server.Specifications
+{
+    public class ProductsWithFiltersForCountSpecification : BaseSpecification<Product>
+    {
+        public ProductsWithFiltersForCountSpecification(ProductSpecificationParams productParams) 
+            : base(p =>
+                (string.IsNullOrEmpty(productParams.Search) || p.Name.ToLower().Contains(productParams.Search)) &&
+                (!productParams.BrandId.HasValue || p.BrandId == productParams.BrandId) &&
+                (!productParams.CategoryId.HasValue || p.CategoryId == productParams.CategoryId)
+            )
+        {
+        }
+    }
+}
