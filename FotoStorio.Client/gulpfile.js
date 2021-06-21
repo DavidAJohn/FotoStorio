2daf362dbafa1e3a5ad7f2cbde1a7d8bd2c6abe7
David John <davidajohn@gmail.com>
2021-06-21 18:49:08 +0100

    Used node to install and configure tailwind css

---
+++ FotoStorio.Client/gulpfile.js
@@ -0,0 +1,28 @@
+const gulp = require('gulp');
+const postcss = require('gulp-postcss');
+const cleanCSS = require('gulp-clean-css');
+const purgecss = require('gulp-purgecss');
+
+gulp.task('css:dev', () => {
+  return gulp.src('./Styles/tailwind.css')
+    .pipe(postcss([
+      require('precss'),
+      require('tailwindcss'),
+      require('autoprefixer')
+    ]))
+    .pipe(gulp.dest('./wwwroot/css/'));
+});
+
+gulp.task('css:prod', () => {
+  return gulp.src('./Styles/tailwind.css')
+    .pipe(sourcemaps.init())
+    .pipe(postcss([
+      require('precss'),
+      require('tailwindcss'),
+      require('autoprefixer')
+    ]))
+    .pipe(purgecss({ content: ['**/*.html', '**/*.razor'] }))
+    .pipe(cleanCSS({ level: 2 }))
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest('./wwwroot/css/'));
+});
---
+++ FotoStorio.Client/tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+    theme: {
+      fontFamily: {
+        sans: ['Graphik', 'sans-serif'],
+        serif: ['Merriweather', 'serif'],
+      },
+      extend: {
+        spacing: {
+          '128': '32rem',
+          '144': '36rem',
+        },
+        borderRadius: {
+          '4xl': '2rem',
+        },
+        colors: {
+            fotoblue: '#111750'
+        }
+      }
+    }
+  }
