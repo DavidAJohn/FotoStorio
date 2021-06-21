a7659bcf4190be4f9b98c1447e4b1206dd1f253e
David John <davidajohn@gmail.com>
2021-06-21 13:04:11 +0100

    Created product specs for unavailable products

---
+++ FotoStorio.Server/Specifications/ProductsUnavailableForCountSpecification.cs
@@ -0,0 +1,18 @@
+﻿using FotoStorio.Server.Helpers;
+using FotoStorio.Shared.Models;
+
+namespace FotoStorio.Server.Specifications
+{
+    public class ProductsUnavailableForCountSpecification : BaseSpecification<Product>
+    {
+        public ProductsUnavailableForCountSpecification(ProductSpecificationParams productParams)
+            : base(p =>
+                (string.IsNullOrEmpty(productParams.Search) || p.Name.ToLower().Contains(productParams.Search)) &&
+                (!productParams.BrandId.HasValue || p.BrandId == productParams.BrandId) &&
+                (!productParams.CategoryId.HasValue || p.CategoryId == productParams.CategoryId) &&
+                p.IsAvailable == false
+            )
+        {
+        }
+    }
+}
