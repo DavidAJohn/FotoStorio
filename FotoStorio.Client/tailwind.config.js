d9471c67c2bc633e2fa151dcb0abb4ff897edc2d
David John <davidajohn@gmail.com>
2021-06-22 22:51:46 +0100

    Began work on layout by adding a navbar

--- FotoStorio.Client/tailwind.config.js
+++ FotoStorio.Client/tailwind.config.js
@@ -1,5 +1,9 @@
 module.exports = {
     theme: {
+      container: {
+        center: true,
+        padding: '2rem'
+      },
       fontFamily: {
         sans: ['Graphik', 'sans-serif'],
         serif: ['Merriweather', 'serif'],
