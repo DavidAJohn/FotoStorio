4305c5e37bc7489a59c65bef9f855b33f5bbf543
David John <davidajohn@gmail.com>
2021-07-09 17:58:22 +0100

    Updated promo carousel on the home page

--- FotoStorio.Client/wwwroot/scripts/initialiseCarousel.js
+++ FotoStorio.Client/wwwroot/scripts/initialiseCarousel.js
@@ -2,9 +2,12 @@ function initialiseCarousel() {
     const promoCarousel = document.querySelector('#promo-carousel');
 
     const carousel = new bootstrap.Carousel(promoCarousel, {
-        interval: 2000
+        interval: 5000 // interval between slides in ms
     });
 
+    // this shifts focus to and from a separate page element 
+    // to help improve consistency of carousel initialisation,
+    // although the user clicking on the page should be sufficient 
     const navElement = document.querySelector('#navbar-top');
     navElement.focus();
     navElement.blur();
