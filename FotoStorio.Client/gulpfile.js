13a8d335b51b53e002b8eb8bdf8bb6e9d4c77ebd
David John <davidajohn@gmail.com>
2021-07-12 20:19:26 +0100

    Updated tailwind gulp config for production

--- FotoStorio.Client/gulpfile.js
+++ FotoStorio.Client/gulpfile.js
@@ -15,14 +15,12 @@ gulp.task('css:dev', () => {
 
 gulp.task('css:prod', () => {
   return gulp.src('./Styles/tailwind.css')
-    .pipe(sourcemaps.init())
     .pipe(postcss([
       require('precss'),
       require('tailwindcss'),
       require('autoprefixer')
     ]))
-    .pipe(purgecss({ content: ['**/*.html', '**/*.razor'] }))
+    .pipe(purgecss({ content: ['**/*.html', '**/*.razor'], defaultExtractor: content => content.match(/[^<>"'`\s]*[^<>"'`\s:]/g) || [] }))
     .pipe(cleanCSS({ level: 2 }))
-    .pipe(sourcemaps.write('.'))
     .pipe(gulp.dest('./wwwroot/css/'));
 });
