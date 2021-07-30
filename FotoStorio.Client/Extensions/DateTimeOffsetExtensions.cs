fdd6c405ce063607c36feeb81f7c948fc74d2f1e
David John <davidajohn@gmail.com>
2021-07-30 17:28:13 +0100

    Added a basic customer orders page

---
+++ FotoStorio.Client/Extensions/DateTimeOffsetExtensions.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace FotoStorio.Client.Extensions
+{
+    public static class DateTimeOffsetExtensions
+    {
+        public static string ToUKStandardDate(this DateTimeOffset dateTime)
+        {
+            // returns date as dd/mm/yy (eg. 30/07/2021)
+            return String.Format("{0:dd/MM/yyyy}", dateTime);
+        }
+    }
+}
