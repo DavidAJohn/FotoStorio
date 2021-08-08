f19725d2f3f32c241d3615bb851616e776a4c94c
David John <davidajohn@gmail.com>
2021-08-08 17:11:11 +0100

    Improved the checkout address form validation messages

--- FotoStorio.Shared/DTOs/AddressDTO.cs
+++ FotoStorio.Shared/DTOs/AddressDTO.cs
@@ -4,24 +4,30 @@ namespace FotoStorio.Shared.DTOs
 {
     public class AddressDTO
     {
-        [Required]
+        [Required(ErrorMessage = "First Name is required")]
+        [Display(Name = "First Name")]
         public string FirstName { get; set; }
         
-        [Required]
+        [Required(ErrorMessage = "Last Name is required")]
+        [Display(Name = "Last Name")]
         public string LastName { get; set; }
         
-        [Required]
+        [Required(ErrorMessage = "Your Street is required")]
+        [Display(Name = "Street")]
         public string Street { get; set; }
         
         public string SecondLine { get; set; }
         
-        [Required]
+        [Required(ErrorMessage = "Your Town/City is required")]
+        [Display(Name = "Town/City")]
         public string City { get; set; }
         
-        [Required]
+        [Required(ErrorMessage = "Your County is required")]
+        [Display(Name = "County")]
         public string County { get; set; }
         
-        [Required]
+        [Required(ErrorMessage = "Your Post Code is required")]
+        [Display(Name = "Post Code")]
         public string PostCode { get; set; }
     }
 }
