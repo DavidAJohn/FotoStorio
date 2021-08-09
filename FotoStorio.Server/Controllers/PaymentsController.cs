067f2a23f04c4e06821f9c726188f83b6c46366d
David John <davidajohn@gmail.com>
2021-08-09 14:54:45 +0100

    Corrected Stripe webhook logging

--- FotoStorio.Server/Controllers/PaymentsController.cs
+++ FotoStorio.Server/Controllers/PaymentsController.cs
@@ -51,17 +51,17 @@ namespace FotoStorio.Server.Controllers
             {
                 case "payment_intent.succeeded":
                     intent = (PaymentIntent)stripeEvent.Data.Object;
-                    _logger.LogInformation("Payment Succeeded: ", intent.Id);
+                    _logger.LogInformation("Payment Succeeded: {Id}", intent.Id);
 
                     order = await _paymentService.UpdateOrderPaymentSucceeded(intent.Id);
-                    _logger.LogInformation("Order updated to payment received: ", order.Id);
+                    _logger.LogInformation("Order updated to payment succeeded: {Id}", order.Id);
                     break;
                 case "payment_intent.payment_failed":
                     intent = (PaymentIntent)stripeEvent.Data.Object;
-                    _logger.LogInformation("Payment Failed: ", intent.Id);
+                    _logger.LogInformation("Payment Failed: {Id}", intent.Id);
 
                     order = await _paymentService.UpdateOrderPaymentFailed(intent.Id);
-                    _logger.LogInformation("Payment failed: ", order.Id);
+                    _logger.LogInformation("Order updated to payment failed: {Id}", order.Id);
                     break;
             }
 
