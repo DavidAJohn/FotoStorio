7da0265987150ae99b06f63867190bb7a7fa875e
David John <davidajohn@gmail.com>
2022-04-24 21:05:04 +0100

    Added Polly to client app to provide connection fault tolerance

--- FotoStorio.Client/Program.cs
+++ FotoStorio.Client/Program.cs
@@ -15,6 +15,9 @@ using System.Collections.Generic;
 using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
+using Polly;
+using Serilog;
+using Polly.Extensions.Http;
 
 namespace FotoStorio.Client
 {
@@ -25,7 +28,10 @@ namespace FotoStorio.Client
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("#app");
 
-            builder.Services.AddHttpClient("FotoStorioAPI", client => client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress + "api/"));
+            builder.Services.AddHttpClient("FotoStorioAPI", c => c.BaseAddress = 
+                new Uri(builder.HostEnvironment.BaseAddress + "api/"))
+                .AddPolicyHandler(GetRetryPolicy())
+                .AddPolicyHandler(GetCircuitBreakerPolicy());
 
             builder.Services.AddScoped<IProductService, ProductService>();
             builder.Services.AddScoped<IOrderService, OrderService>();
@@ -56,6 +62,35 @@ namespace FotoStorio.Client
             builder.Services.AddBlazoredToast();
 
             await builder.Build().RunAsync();
+        }
+
+        private static IAsyncPolicy<HttpResponseMessage> GetRetryPolicy()
+        {
+            // Retry with jitter: https://github.com/App-vNext/Polly/wiki/Retry-with-jitter
+            Random jitter = new Random();
+
+            return HttpPolicyExtensions
+                .HandleTransientHttpError()
+                .WaitAndRetryAsync(
+                    retryCount: 5,
+                    sleepDurationProvider: retryAttempt =>
+                            TimeSpan.FromSeconds(Math.Pow(2, retryAttempt))  // exponential backoff (2, 4, 8, 16, 32 secs)
+                        + TimeSpan.FromMilliseconds(jitter.Next(0, 1000)), // plus some jitter: up to 1 second
+                    onRetry: (exception, retryCount, context) =>
+                    {
+                        Log.Warning("Retry {retryCount} of {policyKey} at {operationKey}, due to: {exception}.",
+                            retryCount, context.PolicyKey, context.OperationKey, exception);
+                    });
+        }
+
+        private static IAsyncPolicy<HttpResponseMessage> GetCircuitBreakerPolicy()
+        {
+            return HttpPolicyExtensions
+                .HandleTransientHttpError()
+                .CircuitBreakerAsync(
+                    handledEventsAllowedBeforeBreaking: 5,
+                    durationOfBreak: TimeSpan.FromSeconds(30)
+                );
         }
     }
 }
