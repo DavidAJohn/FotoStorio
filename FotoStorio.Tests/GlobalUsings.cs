aa3259f53f688ed2ad6c55cb8dd6006c3c2be27e
David John <davidajohn@gmail.com>
2023-09-18 16:49:11 +0100

    Added unit tests for the Products Controller

---
+++ FotoStorio.Tests/GlobalUsings.cs
@@ -0,0 +1,8 @@
+﻿global using FotoStorio.Server.Contracts;
+global using FotoStorio.Server.Helpers;
+global using FotoStorio.Shared.DTOs;
+global using FotoStorio.Shared.Models;
+global using Xunit;
+global using FluentAssertions;
+global using NSubstitute;
+global using AutoFixture;
