91d8a120f560b03f6a3c27904aa7621578fc080b
David John <davidajohn@gmail.com>
2023-11-29 15:51:59 +0000

    Made two small improvements to the HttpContextExtensions class

--- FotoStorio.Server/Extensions/HttpContextExtensions.cs
+++ FotoStorio.Server/Extensions/HttpContextExtensions.cs
@@ -1,42 +1,36 @@
 ﻿using FotoStorio.Shared.Entities;
-using Microsoft.AspNetCore.Http;
-using System;
 using System.Text.Json;
 
-namespace FotoStorio.Server.Extensions
+namespace FotoStorio.Server.Extensions;
+
+public static class HttpContextExtensions
 {
-    public static class HttpContextExtensions
+    public static void AddPaginationResponseHeaders(
+            this HttpContext httpContext,
+            int recordCount,
+            int pageSize,
+            int pageIndex
+        )
     {
-        public static void AddPaginationResponseHeaders(
-                this HttpContext httpContext,
-                int recordCount,
-                int pageSize,
-                int pageIndex
-            )
+        ArgumentNullException.ThrowIfNull(httpContext);
+
+        // calculate total number of pages
+        int totalPages = (int)Math.Ceiling(recordCount / (double)pageSize);
+
+        // are there previous or next pages?
+        bool hasPrevious = pageIndex > 1;
+        bool hasNext = pageIndex < totalPages;
+
+        var paginationHeader = new PagingMetadata
         {
-            if (httpContext == null)
-            {
-                throw new ArgumentNullException(nameof(httpContext));
-            }
+            TotalCount = recordCount,
+            PageIndex = pageIndex,
+            PageSize = pageSize,
+            TotalPages = totalPages,
+            HasPreviousPage = hasPrevious,
+            HasNextPage = hasNext
+        };
 
-            // calculate total number of pages
-            int totalPages = (int)Math.Ceiling(recordCount / (double)pageSize);
-
-            // are there previous or next pages?
-            bool hasPrevious = pageIndex > 1;
-            bool hasNext = pageIndex < totalPages;
-
-            var paginationHeader = new PagingMetadata
-            {
-                TotalCount = recordCount,
-                PageIndex = pageIndex,
-                PageSize = pageSize,
-                TotalPages = totalPages,
-                HasPreviousPage = hasPrevious,
-                HasNextPage = hasNext
-            };
-
-            httpContext.Response.Headers.Add("Pagination", JsonSerializer.Serialize(paginationHeader));
-        }
+        httpContext.Response.Headers.Append("Pagination", JsonSerializer.Serialize(paginationHeader));
     }
 }
