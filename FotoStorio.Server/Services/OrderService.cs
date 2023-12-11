61ac6b1e88a6692e91ac10f9711cdc160d8d4688
David John <davidajohn@gmail.com>
2023-12-11 21:44:03 +0000

    Converted the Order Service class to use a primary constructor

--- FotoStorio.Server/Services/OrderService.cs
+++ FotoStorio.Server/Services/OrderService.cs
@@ -3,82 +3,74 @@ using FotoStorio.Server.Specifications;
 using FotoStorio.Shared.DTOs;
 using FotoStorio.Shared.Models.Orders;
 
-namespace FotoStorio.Server.Services
+namespace FotoStorio.Server.Services;
+
+public class OrderService(IProductRepository productRepository, IOrderRepository orderRepository, IPaymentService paymentService) : IOrderService
 {
-    public class OrderService : IOrderService
+    private readonly IProductRepository _productRepository = productRepository;
+    private readonly IOrderRepository _orderRepository = orderRepository;
+    private readonly IPaymentService _paymentService = paymentService;
+
+    public async Task<Order> CreateOrderAsync(string buyerEmail, OrderCreateDTO order)
     {
-        private readonly IProductRepository _productRepository;
-        private readonly IOrderRepository _orderRepository;
-        private readonly IPaymentService _paymentService;
+        // get items (crucially, with price from db) from the products repository
+        var items = new List<OrderItem>();
 
-        public OrderService(IProductRepository productRepository, IOrderRepository orderRepository, IPaymentService paymentService)
+        var basket = order.Items;
+
+        foreach (var item in basket)
         {
-            _paymentService = paymentService;
-            _orderRepository = orderRepository;
-            _productRepository = productRepository;
-        }
+            var productItem = await _productRepository.GetByIdAsync(item.Product.Id);
+            var itemOrdered = new ProductItemOrdered(productItem.Id, productItem.Name, productItem.ImageUrl);
 
-        public async Task<Order> CreateOrderAsync(string buyerEmail, OrderCreateDTO order)
-        {
-            // get items (crucially, with price from db) from the products repository
-            var items = new List<OrderItem>();
-
-            var basket = order.Items;
-
-            foreach (var item in basket)
-            {
-                var productItem = await _productRepository.GetByIdAsync(item.Product.Id);
-                var itemOrdered = new ProductItemOrdered(productItem.Id, productItem.Name, productItem.ImageUrl);
-
-                // is the product on offer or at regular price?
-                var currentPrice = productItem.SalePrice != 0 && productItem.SalePrice < productItem.Price ? productItem.SalePrice : productItem.Price;
-                
-                var orderItem = new OrderItem(itemOrdered, currentPrice, item.Quantity);
-                items.Add(orderItem);
-            }
-
-            // calculate the total
-            var total = items.Sum(item => item.Price * item.Quantity);
-
-            // check for an existing order
-            var spec = new OrderByPaymentIntentIdSpecification(order.PaymentIntentId);
-            var existingOrder = await _orderRepository.GetEntityWithSpecification(spec);
-
-            if (existingOrder != null)
-            {
-                await _orderRepository.Delete(existingOrder);
-
-                var pi = new PaymentIntentCreateDTO {
-                    Items = order.Items,
-                    PaymentIntentId = order.PaymentIntentId
-                };
-
-                await _paymentService.CreateOrUpdatePaymentIntent(pi);
-            }
-
-            // create the order and save changes
-            var orderToCreate = new Order(items, buyerEmail, order.SendToAddress, total, order.PaymentIntentId);
-            var newOrder = await _orderRepository.Create(orderToCreate);
+            // is the product on offer or at regular price?
+            var currentPrice = productItem.SalePrice != 0 && productItem.SalePrice < productItem.Price ? productItem.SalePrice : productItem.Price;
             
-            // return the new order
-            if (newOrder != null) 
-            {
-                return newOrder;
-            }
-
-            return null;
+            var orderItem = new OrderItem(itemOrdered, currentPrice, item.Quantity);
+            items.Add(orderItem);
         }
 
-        public async Task<Order> GetOrderByIdAsync(int id, string buyerEmail)
+        // calculate the total
+        var total = items.Sum(item => item.Price * item.Quantity);
+
+        // check for an existing order
+        var spec = new OrderByPaymentIntentIdSpecification(order.PaymentIntentId);
+        var existingOrder = await _orderRepository.GetEntityWithSpecification(spec);
+
+        if (existingOrder != null)
         {
-            var spec = new OrdersWithItemsForUserSpecification(id, buyerEmail);
-            return await _orderRepository.GetEntityWithSpecification(spec);
+            await _orderRepository.Delete(existingOrder);
+
+            var pi = new PaymentIntentCreateDTO {
+                Items = order.Items,
+                PaymentIntentId = order.PaymentIntentId
+            };
+
+            await _paymentService.CreateOrUpdatePaymentIntent(pi);
         }
 
-        public async Task<IEnumerable<Order>> GetOrdersForUserAsync(string buyerEmail)
+        // create the order and save changes
+        var orderToCreate = new Order(items, buyerEmail, order.SendToAddress, total, order.PaymentIntentId);
+        var newOrder = await _orderRepository.Create(orderToCreate);
+        
+        // return the new order
+        if (newOrder != null) 
         {
-            var spec = new OrdersWithItemsForUserSpecification(buyerEmail);
-            return await _orderRepository.ListWithSpecificationAsync(spec);
+            return newOrder;
         }
+
+        return null;
+    }
+
+    public async Task<Order> GetOrderByIdAsync(int id, string buyerEmail)
+    {
+        var spec = new OrdersWithItemsForUserSpecification(id, buyerEmail);
+        return await _orderRepository.GetEntityWithSpecification(spec);
+    }
+
+    public async Task<IEnumerable<Order>> GetOrdersForUserAsync(string buyerEmail)
+    {
+        var spec = new OrdersWithItemsForUserSpecification(buyerEmail);
+        return await _orderRepository.ListWithSpecificationAsync(spec);
     }
 }
