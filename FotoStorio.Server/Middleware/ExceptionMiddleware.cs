c0d2d7f2519a7cb2c419a6470405c397c0424abd
David John <davidajohn@gmail.com>
2023-12-13 23:32:47 +0000

    Converted the ExceptionMiddleware class to use a primary constructor with fields removed

--- FotoStorio.Server/Middleware/ExceptionMiddleware.cs
+++ FotoStorio.Server/Middleware/ExceptionMiddleware.cs
@@ -4,26 +4,17 @@ using System.Text.Json;
 
 namespace FotoStorio.Server.Middleware;
 
-public class ExceptionMiddleware
+public class ExceptionMiddleware(RequestDelegate next, ILogger<ExceptionMiddleware> logger)
 {
-    private readonly RequestDelegate _next;
-    private readonly ILogger<ExceptionMiddleware> _logger;
-
-    public ExceptionMiddleware(RequestDelegate next, ILogger<ExceptionMiddleware> logger)
-    {
-        _next = next;
-        _logger = logger;
-    }
-
     public async Task Invoke(HttpContext context)
     {
         try
         {
-            await _next(context);
+            await next(context);
         }
         catch (Exception error)
         {
-            _logger.LogError("Exception: {error}, Message: {message}", error, error.Message);
+            logger.LogError("Exception: {error}, Message: {message}", error, error.Message);
 
             var response = context.Response;
             response.ContentType = "application/json";
