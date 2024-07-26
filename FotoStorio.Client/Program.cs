3344c8c11b1fb26cd582be13f9a90c239b2bcb82
David John <davidajohn@gmail.com>
2024-07-26 14:45:56 +0100

    Switched to Microsoft's native Http Resilience extensions instead of Polly extensions. Closes #26

--- FotoStorio.Client/Program.cs
+++ FotoStorio.Client/Program.cs
@@ -4,12 +4,11 @@ using Blazored.Toast;
 using FotoStorio.Client.Contracts;
 using FotoStorio.Client.Providers;
 using FotoStorio.Client.Services;
-using FotoStorio.Shared.Auth;
 using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.Http.Resilience;
 using Polly;
 using Serilog;
-using Polly.Extensions.Http;
 using FotoStorio.Client;
 using Microsoft.AspNetCore.Components.Web;
 
@@ -17,10 +16,30 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddHttpClient("FotoStorioAPI", c => c.BaseAddress = 
+builder.Services.AddHttpClient("FotoStorioAPI", c => c.BaseAddress =
     new Uri(builder.HostEnvironment.BaseAddress + "api/"))
-    .AddPolicyHandler(GetRetryPolicy())
-    .AddPolicyHandler(GetCircuitBreakerPolicy());
+    .AddResilienceHandler("res-handler", builder =>
+    {
+        builder.AddRetry(new HttpRetryStrategyOptions
+        {
+            MaxRetryAttempts = 5,
+            Delay = TimeSpan.FromSeconds(2),
+            BackoffType = DelayBackoffType.Exponential,
+            UseJitter = true,
+            OnRetry = static args =>
+            {
+                Log.Warning("Retry no. {attemptNumber} at {operationKey}", args.AttemptNumber, args.Context.OperationKey);
+
+                return default;
+            }
+        });
+
+        builder.AddCircuitBreaker(new HttpCircuitBreakerStrategyOptions 
+        { 
+            BreakDuration = TimeSpan.FromSeconds(30),
+            MinimumThroughput = 5,
+        });
+    });
 
 builder.Services.AddScoped<IProductService, ProductService>();
 builder.Services.AddScoped<IOrderService, OrderService>();
@@ -51,33 +70,3 @@ builder.Services.AddBlazoredSessionStorage();
 builder.Services.AddBlazoredToast();
 
 await builder.Build().RunAsync();
-        
-
-IAsyncPolicy<HttpResponseMessage> GetRetryPolicy()
-{
-    // Retry with jitter: https://github.com/App-vNext/Polly/wiki/Retry-with-jitter
-    Random jitter = new Random();
-
-    return HttpPolicyExtensions
-        .HandleTransientHttpError()
-        .WaitAndRetryAsync(
-            retryCount: 5,
-            sleepDurationProvider: retryAttempt =>
-                    TimeSpan.FromSeconds(Math.Pow(2, retryAttempt))  // exponential backoff (2, 4, 8, 16, 32 secs)
-                + TimeSpan.FromMilliseconds(jitter.Next(0, 1000)), // plus some jitter: up to 1 second
-            onRetry: (exception, retryCount, context) =>
-            {
-                Log.Warning("Retry {retryCount} of {policyKey} at {operationKey}, due to: {exception}.",
-                    retryCount, context.PolicyKey, context.OperationKey, exception);
-            });
-}
-
-IAsyncPolicy<HttpResponseMessage> GetCircuitBreakerPolicy()
-{
-    return HttpPolicyExtensions
-        .HandleTransientHttpError()
-        .CircuitBreakerAsync(
-            handledEventsAllowedBeforeBreaking: 5,
-            durationOfBreak: TimeSpan.FromSeconds(30)
-        );
-}
