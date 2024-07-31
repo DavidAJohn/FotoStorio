560f8c5dc8239ac807d633a3d3d127f3432c9c78
David John <davidajohn@gmail.com>
2024-07-31 15:09:03 +0100

    Updated the auth state provider to check for expired jwts. Closes #33

--- FotoStorio.Client/Providers/ApiAuthenticationStateProvider.cs
+++ FotoStorio.Client/Providers/ApiAuthenticationStateProvider.cs
@@ -1,102 +1,121 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net.Http;
+using Blazored.LocalStorage;
+using Microsoft.AspNetCore.Components.Authorization;
 using System.Net.Http.Headers;
 using System.Security.Claims;
 using System.Text.Json;
-using System.Threading.Tasks;
-using Blazored.LocalStorage;
-using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.IdentityModel.JsonWebTokens;
 
-namespace FotoStorio.Client.Providers
+namespace FotoStorio.Client.Providers;
+
+public class ApiAuthenticationStateProvider : AuthenticationStateProvider
 {
-    public class ApiAuthenticationStateProvider : AuthenticationStateProvider
+    private readonly HttpClient _httpClient;
+    private readonly ILocalStorageService _localStorage;
+
+    public ApiAuthenticationStateProvider(HttpClient httpClient, ILocalStorageService localStorage)
     {
-        private readonly HttpClient _httpClient;
-        private readonly ILocalStorageService _localStorage;
+        _httpClient = httpClient;
+        _localStorage = localStorage;
+    }
+    public override async Task<AuthenticationState> GetAuthenticationStateAsync()
+    {
+        var savedToken = await _localStorage.GetItemAsync<string>("authToken");
 
-        public ApiAuthenticationStateProvider(HttpClient httpClient, ILocalStorageService localStorage)
+        if (string.IsNullOrWhiteSpace(savedToken))
         {
-            _httpClient = httpClient;
-            _localStorage = localStorage;
+            return new AuthenticationState(new ClaimsPrincipal(new ClaimsIdentity()));
         }
-        public override async Task<AuthenticationState> GetAuthenticationStateAsync()
-        {
-            var savedToken = await _localStorage.GetItemAsync<string>("authToken");
 
-            if (string.IsNullOrWhiteSpace(savedToken))
+        // if the token has already expired or the expiry date is missing, remove the token
+        if (ExtractExpiryDateFromJwt(savedToken) < DateTime.UtcNow || 
+            ExtractExpiryDateFromJwt(savedToken) == DateTime.MinValue) // DateTime.MinValue means the token does not have an expiry date
+        {
+            await _localStorage.RemoveItemAsync("authToken");
+            return new AuthenticationState(new ClaimsPrincipal(new ClaimsIdentity()));
+        }
+
+        _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("bearer", savedToken);
+
+        return new AuthenticationState(new ClaimsPrincipal(new ClaimsIdentity(ParseClaimsFromJwt(savedToken), "jwt")));
+    }
+
+    public void MarkUserAsAuthenticated(string email)
+    {
+        var authenticatedUser = new ClaimsPrincipal(new ClaimsIdentity(new[] { new Claim(ClaimTypes.Name, email) }, "apiauth"));
+        var authState = Task.FromResult(new AuthenticationState(authenticatedUser));
+        NotifyAuthenticationStateChanged(authState);
+    }
+
+    public void MarkUserAsLoggedOut()
+    {
+        var anonymousUser = new ClaimsPrincipal(new ClaimsIdentity());
+        var authState = Task.FromResult(new AuthenticationState(anonymousUser));
+        NotifyAuthenticationStateChanged(authState);
+    }
+
+    private static IEnumerable<Claim> ParseClaimsFromJwt(string jwt)
+    {
+        var claims = new List<Claim>();
+        var payload = jwt.Split('.')[1];
+        var jsonBytes = ParseBase64WithoutPadding(payload);
+
+        var keyValuePairs = JsonSerializer.Deserialize<Dictionary<string, object>>(jsonBytes);
+
+        ExtractRolesFromJwt(claims, keyValuePairs);
+
+        claims.AddRange(keyValuePairs.Select(kvp => new Claim(kvp.Key, kvp.Value.ToString())));
+
+        return claims;
+    }
+
+    private static void ExtractRolesFromJwt(List<Claim> claims, Dictionary<string, object> keyValuePairs)
+    {
+        keyValuePairs.TryGetValue(ClaimTypes.Role, out object roles);
+
+        if (roles != null)
+        {
+            var parsedRoles = roles.ToString().Trim().TrimStart('[').TrimEnd(']').Split(',');
+
+            if (parsedRoles.Length > 1)
             {
-                return new AuthenticationState(new ClaimsPrincipal(new ClaimsIdentity()));
-            }
-
-            _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("bearer", savedToken);
-
-            return new AuthenticationState(new ClaimsPrincipal(new ClaimsIdentity(ParseClaimsFromJwt(savedToken), "jwt")));
-        }
-
-        public void MarkUserAsAuthenticated(string email)
-        {
-            var authenticatedUser = new ClaimsPrincipal(new ClaimsIdentity(new[] { new Claim(ClaimTypes.Name, email) }, "apiauth"));
-            var authState = Task.FromResult(new AuthenticationState(authenticatedUser));
-            NotifyAuthenticationStateChanged(authState);
-        }
-
-        public void MarkUserAsLoggedOut()
-        {
-            var anonymousUser = new ClaimsPrincipal(new ClaimsIdentity());
-            var authState = Task.FromResult(new AuthenticationState(anonymousUser));
-            NotifyAuthenticationStateChanged(authState);
-        }
-
-        private static IEnumerable<Claim> ParseClaimsFromJwt(string jwt)
-        {
-            var claims = new List<Claim>();
-            var payload = jwt.Split('.')[1];
-            var jsonBytes = ParseBase64WithoutPadding(payload);
-
-            var keyValuePairs = JsonSerializer.Deserialize<Dictionary<string, object>>(jsonBytes);
-
-            ExtractRolesFromJwt(claims, keyValuePairs);
-
-            claims.AddRange(keyValuePairs.Select(kvp => new Claim(kvp.Key, kvp.Value.ToString())));
-
-            return claims;
-        }
-
-        private static void ExtractRolesFromJwt(List<Claim> claims, Dictionary<string, object> keyValuePairs)
-        {
-            keyValuePairs.TryGetValue(ClaimTypes.Role, out object roles);
-
-            if (roles != null)
-            {
-                var parsedRoles = roles.ToString().Trim().TrimStart('[').TrimEnd(']').Split(',');
-
-                if (parsedRoles.Length > 1)
+                foreach (var parsedRole in parsedRoles)
                 {
-                    foreach (var parsedRole in parsedRoles)
-                    {
-                        claims.Add(new Claim(ClaimTypes.Role, parsedRole.Trim('"')));
-                    }
+                    claims.Add(new Claim(ClaimTypes.Role, parsedRole.Trim('"')));
                 }
-                else
-                {
-                    claims.Add(new Claim(ClaimTypes.Role, parsedRoles[0]));
-                }
-
-                keyValuePairs.Remove(ClaimTypes.Role);
             }
-        }
-
-        private static byte[] ParseBase64WithoutPadding(string base64)
-        {
-            switch (base64.Length % 4)
+            else
             {
-                case 2: base64 += "=="; break;
-                case 3: base64 += "="; break;
+                claims.Add(new Claim(ClaimTypes.Role, parsedRoles[0]));
             }
-            
-            return Convert.FromBase64String(base64);
+
+            keyValuePairs.Remove(ClaimTypes.Role);
         }
+    }
+
+    private static byte[] ParseBase64WithoutPadding(string base64)
+    {
+        switch (base64.Length % 4)
+        {
+            case 2: base64 += "=="; break;
+            case 3: base64 += "="; break;
+        }
+        
+        return Convert.FromBase64String(base64);
+    }
+
+    private static DateTime ExtractExpiryDateFromJwt(string jwt)
+    {
+        try
+        {
+            var tokenHandler = new JsonWebTokenHandler();
+            var jsonWebToken = tokenHandler.ReadJsonWebToken(jwt);
+            var expiryDate = jsonWebToken.ValidTo;
+
+            return expiryDate; // will be DateTime.MinValue if the token does not have an expiry date
+        }
+        catch (Exception)
+        {
+            return DateTime.MinValue;
+        } 
     }
 }
